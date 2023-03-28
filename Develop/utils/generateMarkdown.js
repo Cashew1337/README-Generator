dc02f791051e4079f434b44bcdce8b7fb931c158
Mr. Macro <mr.matt.caschetto@gmail.com>
2023-03-28 17:57:42 -0400

    Commit to push README and updated markdown.js.

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -29,7 +29,7 @@ function renderLicenseLink(license) {
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  if (license !== null) {
+  if (license !== none) {
     return '## License'
   } else {
     return ''
@@ -37,7 +37,7 @@ function renderLicenseSection(license) {
 }
 
 function renderLicenseToc(license) {
-  if (license !== null) {
+  if (license !== none) {
     return '[License](#license)'
   } else {
     return ''
@@ -50,12 +50,19 @@ function generateMarkdown(data) {
 ${renderLicenseBadge(data.license)}
 
   ## Table of Contents
+
   [Description](#description)
+
   [Installation](#installation)
+
   [Usage](#usage)
+
   ${renderLicenseToc(data.license)}
+
   [Contribution](#contribution)
+
   [Tests](#test)
+  
   [Questions](#questions)
 
 ## Description
